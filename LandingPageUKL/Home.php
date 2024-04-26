b0ee40d871f8482e957651ed4affe7809ecd6128
Fathrhys <151694431+Fathrhys@users.noreply.github.com>
2024-04-26 10:00:20 +0700

    Add files via upload

---
+++ LandingPageUKL/Home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="Home.css">
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="logo"><a href="">SpedyGo</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Order</a></li>
+                    <li><a href="">Riwayat</a></li>
+                    <li><a href="">Tentang Kami</a></li>
+                    <li><a href="">Akun</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="navbar">
+        <section id="home">
+            <h1>Selamat Datang</h1>
+            <h2>SpedyGo</h2>
+        </section>
+    
+</div>
+</body>
+</html>
